67c283446d79000c6802e30f23224606627276f7
phtf92 <phtf92@gmail.com>
2019-11-19 18:25:18 -0300

    Exercise 3

--- exercises/4_1_/Ex_4_1.js
+++ exercises/4_1_/Ex_4_1.js
@@ -15,3 +15,10 @@ if (x > y && x > z) { console.log("O maior número é: ", x) }
 else if (y > x && y > z) { console.log("O maior número é:", y) }
 else if (z > x && z > y) { console.log("O maior número é:", z) }
 else { console.log("Não tem número maior!") };
+
+//Exercício 3
+let c = 4;
+let d = 5;
+if (c == d) { console.log("Os números são iguais") }
+else if (c > d) { console.log(x, "é maior que", d) }
+else { console.log(d, 'é maior que', c) }
