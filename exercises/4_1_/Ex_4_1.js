03b30af2d74b3e3f579ae15a73a03717d067b0af
phtf92 <phtf92@gmail.com>
2019-11-19 18:18:28 -0300

    Exercise 2

--- exercises/4_1_/Ex_4_1.js
+++ exercises/4_1_/Ex_4_1.js
@@ -6,3 +6,12 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+//Exercício 2
+let x = 5
+let y = 7
+let z = 9
+if (x > y && x > z) { console.log("O maior número é: ", x) }
+else if (y > x && y > z) { console.log("O maior número é:", y) }
+else if (z > x && z > y) { console.log("O maior número é:", z) }
+else { console.log("Não tem número maior!") };
