a29b6b750e2ebde29086728a91e16b05b8dc53a2
phtf92 <phtf92@gmail.com>
2019-11-19 19:06:26 -0300

    Exercise 7

--- exercises/4_1_/Ex_4_1.js
+++ exercises/4_1_/Ex_4_1.js
@@ -65,3 +65,14 @@ switch (xadrez) {
     default:
         console.log("Error");
 }
+
+//Exercício 7
+let nota = 91;
+
+if (nota < 0 || nota > 100) { console.log("Nota inválida") }
+else if (nota >= 90) { console.log("A") }
+else if (nota < 90 && nota >= 80) { console.log("B") }
+else if (nota < 80 && nota >= 70) { console.log("C") }
+else if (nota < 70 && nota >= 60) { console.log("D") }
+else if (nota < 60 && nota >= 50) { console.log("E") }
+else { console.log("F") }
