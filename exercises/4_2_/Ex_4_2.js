2a6d09cb2dc2fe6e037a42a679a42bd7c080e6f3
phtf92 <phtf92@gmail.com>
2019-11-20 00:42:14 -0300

    exercício 3

--- exercises/4_2_/Ex_4_2.js
+++ exercises/4_2_/Ex_4_2.js
@@ -1,4 +1,6 @@
 //Exercício 1
+console.log("Ex1");
+console.log("");
 let n = 5;
 let comprimento = []
 
@@ -11,3 +13,18 @@ while (i < n) {
     console.log(linha);
     i++;
 }
+
+//Exercicio 2
+console.log("");
+console.log("Ex2");
+console.log("");
+n = 5;
+i = 0;
+comprimento = []
+linha = null;
+for (i; i < n; i++) {
+    comprimento.push('*')
+    linha = comprimento.join('');
+    console.log(linha);
+}
+
