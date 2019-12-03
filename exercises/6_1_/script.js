d5d2abbb1dc3a6ced0eb7ae6ce2bcda637f45075
phtf92 <phtf92@gmail.com>
2019-12-03 19:15:55 -0300

    Parte 3

--- exercises/6_1_/script.js
+++ exercises/6_1_/script.js
@@ -6,11 +6,22 @@ let estados = ['Acre (AC)', 'Alagoas (AL)', 'Amapá (AP)', 'Amazonas (AM)', 'Bah
 for (let i = 0; i < 27; i++) {
     let addEstado = document.createElement('option');
     addEstado.innerHTML = estados[i];
+    addEstado.value = estados[i];
     document.getElementsByTagName('select')[0].appendChild(addEstado);
 }
 let enviar2 = document.getElementsByTagName('button')[1];
 enviar2.addEventListener('click', function () {
-    let string = document.getElementById('dataDeComeco').value
+    event.preventDefault();
+    let caminhoInput = document.getElementsByTagName('input');
+    let comprimento = caminhoInput.length;
+    let divPai = document.getElementsByTagName('div')[0];
+    for (let i=0; i<comprimento; i++){
+        let addDiv = document.createElement('div');
+        addDiv.innerHTML = caminhoInput[i].value;
+        divPai.appendChild(addDiv);
+
+    }
+    let string = document.getElementById('dataDeComeco').value;
     let arranjo = string.split('/');
     if (arranjo[0] < 0 || arranjo[0] > 31) alert('O dia do mês deve estar entre 1 e 31')
     if (arranjo[1] < 0 || arranjo[1] > 12) alert('O mês deve estar entre 1 e 12');
