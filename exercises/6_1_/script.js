72027623456d83ba20ad64485e26a73df3bb3993
phtf92 <phtf92@gmail.com>
2019-12-03 18:22:01 -0300

    Alterei coisas de regeX

--- exercises/6_1_/script.js
+++ exercises/6_1_/script.js
@@ -8,3 +8,11 @@ for (let i = 0; i < 27; i++) {
     addEstado.innerHTML = estados[i];
     document.getElementsByTagName('select')[0].appendChild(addEstado);
 }
+let enviar2 = document.getElementsByTagName('button')[1];
+enviar2.addEventListener('click', function () {
+    let string = document.getElementById('dataDeComeco').value
+    let arranjo = string.split('/');
+    if (arranjo[0] < 0 || arranjo[0] > 31) alert('O dia do mês deve estar entre 1 e 31')
+    if (arranjo[1] < 0 || arranjo[1] > 12) alert('O mês deve estar entre 1 e 12');
+    if (arranjo[2] < 0) alert('Ano deve ser positivo');
+})
