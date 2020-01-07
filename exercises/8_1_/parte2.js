8f592bfb1b1bb54bde172e2ef327091dbb22de13
phtf92 <phtf92@gmail.com>
2020-01-07 18:34:22 -0300

    Prt 2 ex 2

--- exercises/8_1_/parte2.js
+++ exercises/8_1_/parte2.js
@@ -13,5 +13,20 @@ console.log(`O fatorial é ${fatorial(1)}`);
 const fatorialRecursivo = b => {
     return resultado = b > 1 ? b * fatorialRecursivo(b - 1) : 1
 }
-
 console.log(fatorialRecursivo(5));
+
+//Parte 2 - ex2
+
+const maiorPalavra = c => {
+    const arranjoPalavras = c.split(' ');
+    let maiorPalavra = arranjoPalavras[0];
+    for (let i = 0; i < arranjoPalavras.length - 1; i++) {
+        if (arranjoPalavras[i].length > arranjoPalavras[i + 1].length) {
+            maiorPalavra = arranjoPalavras[i]
+        }
+    }
+    if (maiorPalavra.length < arranjoPalavras[arranjoPalavras.length - 1].length) { maiorPalavra = arranjoPalavras[arranjoPalavras.length - 1] };
+    return maiorPalavra;
+}
+
+console.log(maiorPalavra("Antônio foi no banheiro e não sabemos o que acontxxxeceu"));
