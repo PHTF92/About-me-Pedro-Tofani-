035a15bb1ac465e16196d9413cd5f4cf6a2f18d2
phtf92 <phtf92@gmail.com>
2020-01-07 23:14:32 -0300

    Troca de jogadores entre os times

--- projetos/Sorteio time/script.js
+++ projetos/Sorteio time/script.js
@@ -1,1 +1,14 @@
-// Iniciando JS
+// let contadorTroca = false;
+
+document.querySelector('#bordaMatriz').addEventListener('click', () => {
+    event.target.classList.toggle('selecionado');
+    if (document.getElementsByClassName('selecionado').length == 2) {
+        let j1 = document.getElementsByClassName('selecionado')[0].innerHTML
+        let j2 = document.getElementsByClassName('selecionado')[1].innerHTML
+        document.getElementsByClassName('selecionado')[0].innerHTML = j2
+        document.getElementsByClassName('selecionado')[1].innerHTML = j1
+        document.getElementsByClassName('selecionado')[1].classList.remove('selecionado')
+        document.getElementsByClassName('selecionado')[0].classList.remove('selecionado')
+    }
+});
+
