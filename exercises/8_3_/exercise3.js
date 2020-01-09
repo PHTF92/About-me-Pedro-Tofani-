4d3117179d8010b5eee255f0841bb2c1b83413c4
phtf92 <phtf92@gmail.com>
2020-01-09 18:32:28 -0300

    ex3

--- exercises/8_3_/exercise3.js
+++ exercises/8_3_/exercise3.js
@@ -16,6 +16,6 @@ const gunnar = {
 
 // complete a assinatura da função abaixo
 
-const personLikes = (item) => `${item.name} is ${item.age} years old and likes ${item.likes.join(", ")}.`
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(", ")}.`
 assert.equal(personLikes(alex), "Alex is 26 years old and likes fly fishing.")
 assert.equal(personLikes(gunnar), "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.")
