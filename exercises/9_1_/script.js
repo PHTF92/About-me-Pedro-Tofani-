b2eb3743c0c3454422998b33a26148f706fb3fee
phtf92 <phtf92@gmail.com>
2020-01-16 18:31:56 -0300

    Colocando as propriedades escolhidas na página

--- exercises/9_1_/script.js
+++ exercises/9_1_/script.js
@@ -1,8 +1,6 @@
-const botao = document.getElementById('submit');
-
 
 function ftipoFonte(tipo) {
-localStorage.setItem('tipoFonte', tipo)
+    localStorage.setItem('tipoFonte', tipo)
 }
 
 function fcorFundo(cor) {
@@ -17,7 +15,19 @@ function fespLinhas(linha) {
     localStorage.setItem('estLinha', linha)
 }
 
-function ftamanhoFonte(){
+function ftamanhoFonte() {
     const tamanhoFonte = document.getElementById('tamanhoFonte').value
     localStorage.setItem('tamanhoFonte', tamanhoFonte)
 }
+
+document.getElementsByTagName('body')[0].style.backgroundColor = localStorage.getItem('corFundo')
+
+const artigo = document.getElementsByTagName('p')[0].style
+
+artigo.fontSize = localStorage.getItem('tamanhoFonte')
+
+artigo.color = localStorage.getItem('corTexto')
+
+artigo.lineHeight = localStorage.getItem('estLinha')
+
+artigo.fontFamily = localStorage.getItem('tipoFonte')
