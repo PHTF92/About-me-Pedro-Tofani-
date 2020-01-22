d508f69154522af0f357e6061babafa3abe335a0
phtf92 <phtf92@gmail.com>
2020-01-22 19:19:10 -0300

    Ex4

---
+++ exercises/10_1/ex4.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+    return arr.filter((elem) => elem !== item);
+}
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4])
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4])
+// Verifique que o array passado por parâmetro não sofreu alterações
+const array = [1, 2, 3, 4]
+assert.equal(() => {
+    myRemove(array, 3)
+    return array
+}, array)
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
