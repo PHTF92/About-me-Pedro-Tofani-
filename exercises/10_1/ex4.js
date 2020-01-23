440705708f5a58fe01bc43f54b6d2d09674c1305
phtf92 <phtf92@gmail.com>
2020-01-23 12:40:45 -0300

    Ex 4

--- exercises/10_1/ex4.js
+++ exercises/10_1/ex4.js
@@ -10,9 +10,7 @@ assert.deepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4])
 assert.notEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4])
 // Verifique que o array passado por parâmetro não sofreu alterações
 const array = [1, 2, 3, 4]
-assert.equal(() => {
-    myRemove(array, 3)
-    return array
-}, array)
+myRemove(array);
+assert.deepStrictEqual(array, [1, 2, 3, 4])
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
