6a264fd360d3dca0598702b670b979c1b81f392c
phtf92 <phtf92@gmail.com>
2020-01-24 18:01:10 -0300

    ex12

--- exercises/10_2/js-exercises-tdd-master/II.write-tests/06-largest-number/largest-number.test.js
+++ exercises/10_2/js-exercises-tdd-master/II.write-tests/06-largest-number/largest-number.test.js
@@ -1,3 +1,11 @@
+const maiorNumero = require('./largest-number.js');
+
+test('Pegar o maior número', ()=> {
+    const array = [3, 21, 88, 4, 36];
+    const resultado = maiorNumero(array)
+    expect(resultado).toEqual(88)
+    expect(array).toEqual([3, 21, 88, 4, 36])
+}) 
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
