dc2d821d17892d55b45bdda719098edca43c8914
phtf92 <phtf92@gmail.com>
2020-01-24 17:48:43 -0300

    Ex10

--- exercises/10_2/js-exercises-tdd-master/II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ exercises/10_2/js-exercises-tdd-master/II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -1,6 +1,12 @@
 var largerThanTen = require("./numbersGreaterThan10");
 
-test("Get numbers greater than 10", function() {});
+test("Get numbers greater than 10", function () {
+    const entrada = [4, 10, 32, 9, 21];
+    resultado = largerThanTen(entrada);
+    expect(resultado).toEqual([32, 21]);
+
+
+});
 
 // input: [4, 10, 32, 9, 21];
 // expected output: [32, 21];
