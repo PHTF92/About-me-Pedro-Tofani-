2fd3bae30202de7df7c93216396e30a7530a9933
phtf92 <phtf92@gmail.com>
2020-01-24 18:59:39 -0300

    Ex15

--- exercises/10_2/js-exercises-tdd-master/I.pass-tests/08-get-average/get-average.js
+++ exercises/10_2/js-exercises-tdd-master/I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,17 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+const media = (numerosArranjos) => {
+    let unidades = 0;
+    let soma = 0;
+    numerosArranjos.forEach(elemento => {
+        if (typeof elemento === 'number') {
+            unidades += 1;
+            soma += elemento
+        }
+    })
+    return soma/unidades
+}
+
+module.exports = media;
