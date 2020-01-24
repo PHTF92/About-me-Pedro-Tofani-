568329c94518d5cb98edd0f2a06ce84f960ad7b6
phtf92 <phtf92@gmail.com>
2020-01-24 18:08:49 -0300

    ex13

--- exercises/10_2/js-exercises-tdd-master/II.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ exercises/10_2/js-exercises-tdd-master/II.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,11 @@
+const numerosImpares = require('./get-even-numbers.js')
+
+test('Pegar os números ímprares', () => {
+    const arranjo = [22, 13, 73, 82, 4];
+    const resultado = numerosImpares(arranjo);
+    expect(resultado).toEqual([22, 82, 4])
+})
+
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
