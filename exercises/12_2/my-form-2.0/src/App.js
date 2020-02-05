bb9ef9f53edaad86fa34e39199c1e94a34452c5e
phtf92 <phtf92@gmail.com>
2020-02-05 09:51:20 -0300

    add campos formulario

--- exercises/12_2/my-form-2.0/src/App.js
+++ exercises/12_2/my-form-2.0/src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      nome: '',
+      cpf: '',
+      endereco: '',
+      cidade: '',
+      estado: '',
+      tipo: '',
+      curriculo: '',
+      cargo: '',
+      descricao: '',
+      formErrors: { email: '', password: '' }
+    }
+  }
+  changeHandler = event => {
+    const { name, value } = event.target;
+    this.setState((state) => {
+      return ({
+        [name]: value
+      })
+    })
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <fieldset>
+          <legend>Formulário</legend>
+          <div>Nome <input
+            type='text'
+            name='nome'
+            value={this.state.nome}
+            onChange={this.changeHandler}
+          /></div>
+          <div>E-mail <input
+            type='email'
+            name='email'
+            value={this.state.email}
+            onChange={this.changeHandler}
+          /></div>
+          <div>CPF <input
+            type='number'
+            name='cpf'
+            value={this.state.cpf}
+            onChange={this.changeHandler}
+          /></div>
+          <div>Endereço <input
+            type='text'
+            name='endereco'
+            value={this.state.endereco}
+            onChange={this.changeHandler}
+          /></div>
+          <div>Cidade <input
+            type='text'
+            name='cidade'
+            value={this.state.cidade}
+            onChange={this.changeHandler}
+          /></div>
+          <div>Estado</div>
+          <div></div>
+        </fieldset>
+        <fieldset>
+          <legend>Informações profissionais</legend>
+          <div>Resumo do currículo <textarea></textarea></div>
+          <div>Cargo <textarea></textarea></div>
+          <div>Descrição do cargo <textarea></textarea></div>
+        </fieldset>
+        <button>Submeter!</button><br></br>
+        <button>Limpar</button>
+      </div>
+    );
+  }
 }
 
 export default App;
