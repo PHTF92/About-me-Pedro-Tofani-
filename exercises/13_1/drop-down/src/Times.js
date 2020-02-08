5a9ef28b7b93ed3cd9c9d46b76711a72e2c5ad86
phtf92 <phtf92@gmail.com>
2020-02-08 16:01:56 -0300

    Caixa com os times

--- exercises/13_1/drop-down/src/Times.js
+++ exercises/13_1/drop-down/src/Times.js
@@ -4,21 +4,39 @@ class Times extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            valorCaixa: '',
-            clicado: false
+            valorCaixa: this.props.children[0].item,
+            clicado: true
         };
     }
+
+    botaoClique = () => {
+        if (this.state.clicado) this.setState({ clicado: false })
+        else this.setState({ clicado: true })
+    }
+
+    selectTime = (event) => {
+        this.setState({ valorCaixa: event.target.value })
+    }
+
+    renderizarLista = () => {
+        const arrTimes = this.props.children.map(time => time.item)
+        if (this.state.clicado) {
+            return (
+                <select size="4">
+                    {arrTimes.map(time =>
+                        <option onClick={this.selectTime} key={time} value={time}>{time}</option>
+                    )}
+                </select>
+            )
+        }
+    }
+    
     render() {
+        console.log(this.state)
         return (
             <div>
-                <button>oi</button><br></br>
-                <select size="4">
-                    <option>Apple</option>
-                    <option>Orange</option>
-                    <option>Pineapple</option>
-                    <option>Banana</option>
-                </select>
-                <p>{this.props.children[0].item}</p>
+                <button onClick={this.botaoClique}>{this.state.valorCaixa}</button><br></br>
+                {this.renderizarLista()}
             </div>
         )
     }
