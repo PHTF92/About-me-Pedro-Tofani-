6b6bc4e65cb00d7902fd2be47d3fec025d37c20e
phtf92 <phtf92@gmail.com>
2020-02-10 15:35:56 -0300

    Ex parte 1

--- exercises/13_2/ex13-2/src/App.js
+++ exercises/13_2/ex13-2/src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 0
+    }
+    this.setNewNumber = this.setNewNumber.bind(this);
+  }
+
+  setNewNumber() {
+    this.setState({ data: this.state.data + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.setNewNumber}>INCREMENT</button>
+        <Content myNumber={this.state.data} />
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+
+  componentDidMount() {
+    alert('O componente foi montado')
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.myNumber % 3 === 0
+  }
+
+  UNSAFE_componentWillUpdate() {
+    alert('Novas props')
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem("dados", this.state.data)
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.myNumber}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
