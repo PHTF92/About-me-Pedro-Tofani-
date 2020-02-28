c28c97ef9fc6b884895c92adc20618af2c32387a
phtf92 <phtf92@gmail.com>
2020-02-28 20:16:26 -0300

    Testes

--- exercises/15_1/jogo-da-velha/src/App.js
+++ exercises/15_1/jogo-da-velha/src/App.js
@@ -10,69 +10,77 @@ class App extends React.Component {
     };
   }
 
-handleClick = (e) => {
-  const { jogador, velha } = this.state
-  const { className, id } = e.target
-  const marcador = (jogador % 2) ? ['X', 0] : ['O', 1]
-  if (this.state[className]) return '';
-  this.setState(() => {
-    const novaVelha = velha;
-    novaVelha.splice(id - 1, 1, marcador[0]);
-    return {[className]: marcador[0], jogador: marcador[1], velha: novaVelha }})
-} 
+  handleClick = (e) => {
+    const { jogador, velha } = this.state
+    const { className, id } = e.target
+    const marcador = (jogador % 2) ? ['X', 0] : ['O', 1]
+    if (this.state[className]) return '';
+    this.setState(() => {
+      const novaVelha = velha;
+      novaVelha.splice(id - 1, 1, marcador[0]);
+      return { [className]: marcador[0], jogador: marcador[1], velha: novaVelha }
+    })
+  }
 
-diagonal(){
-  const { velha } = this.state;
-  const d1 = (velha[0] === velha[4] && velha[4] === velha[8] && velha[0] !== '-')
-  const d2 = (velha[2] === velha[4] && velha[4] === velha[6] && velha[2] !== '-')
-  return (d1 || d2);
-}
+  diagonal() {
+    const { velha } = this.state;
+    const d1 = (velha[0] === velha[4] && velha[4] === velha[8] && velha[0] !== '-')
+    const d2 = (velha[2] === velha[4] && velha[4] === velha[6] && velha[2] !== '-')
+    return (d1 || d2);
+  }
 
-vertical(){
-  const { velha } = this.state;
-  const v1 = (velha[0] === velha[3] && velha[0] === velha[6] && velha[0] !== '-')
-  const v2 = (velha[1] === velha[4] && velha[1] === velha[7] && velha[1] !== '-')
-  const v3 = (velha[2] === velha[5] && velha[2] === velha[8] && velha[2] !== '-')
-  return (v1 || v2 || v3)
-}
+  vertical() {
+    const { velha } = this.state;
+    const v1 = (velha[0] === velha[3] && velha[0] === velha[6] && velha[0] !== '-')
+    const v2 = (velha[1] === velha[4] && velha[1] === velha[7] && velha[1] !== '-')
+    const v3 = (velha[2] === velha[5] && velha[2] === velha[8] && velha[2] !== '-')
+    return (v1 || v2 || v3)
+  }
 
-horizontal(){
-  const { velha } = this.state;
-  const h1 = (velha[0] === velha[1] && velha[0] === velha[2] && velha[0] !== '-')
-  const h2 = (velha[3] === velha[4] && velha[3] === velha[5] && velha[3] !== '-')
-  const h3 = (velha[6] === velha[7] && velha[6] === velha[8] && velha[6] !== '-')
-  return (h1 || h2 || h3)
-}
+  horizontal() {
+    const { velha } = this.state;
+    const h1 = (velha[0] === velha[1] && velha[0] === velha[2] && velha[0] !== '-')
+    const h2 = (velha[3] === velha[4] && velha[3] === velha[5] && velha[3] !== '-')
+    const h3 = (velha[6] === velha[7] && velha[6] === velha[8] && velha[6] !== '-')
+    return (h1 || h2 || h3)
+  }
 
-verificaVitoria () {
-  const a = this.diagonal();
-  const b = this.vertical();
-  const c = this.horizontal();
-  if ( a || b || c ) return <div>Ganhou!</div>
-  return ''
-}
+  verificaVitoria() {
+    const a = this.diagonal();
+    const b = this.vertical();
+    const c = this.horizontal();
+    if (a || b || c) this.setState({ endGame: true })
+    if (a || b || c) return <div>Ganhou!</div>
+    return ''
+  }
 
-crateDivs () {
-  const numeros = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-  return (
-    <div className="container">
-      {numeros.map(numero => {
-        return <div key={numero} id={numero} onClick={this.handleClick} data-testid={`teste${numero}`}
-          casa={numero} className={`div${numero}`}>{this.state[`div${numero}`]}</div> 
-      })}
-    </div>
-  )
-}
-  
-render(){
-  console.log(this.state)
-  return (
-    <div>
-      {this.verificaVitoria()}
-      {this.crateDivs()}
-    </div>
-  );
-}
+  crateDivs() {
+    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return (
+      <div className="container">
+        {numeros.map(numero => {
+          return <div key={numero} id={numero} onClick={this.handleClick} data-testid={`teste${numero}`}
+            casa={numero} className={`div${numero}`}>{this.state[`div${numero}`]}</div>
+        })}
+      </div>
+    )
+  }
+
+  shouldComponentUpdate() {
+    const { endGame } = this.state;
+    if (endGame) return false;
+    return true
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        {this.verificaVitoria()}
+        {this.crateDivs()}
+      </div>
+    );
+  }
 }
 
 export default App;
--- exercises/15_1/jogo-da-velha/src/App.test.js
+++ exercises/15_1/jogo-da-velha/src/App.test.js
@@ -1,9 +1,83 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup, waitForDomChange, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup);
+const divs = ['teste1', 'teste2', 'teste3', 'teste4', 'teste5', 'teste6', 'teste6', 'teste7', 'teste8', 'teste9']
+
+describe('Common Path', () => {
+  test("Renders the empity game", () => {
+    const { getByTestId } = render(<App />);
+    divs.map(teste => {
+      expect(getByTestId(teste)).toHaveTextContent('')
+    })
+  })
+  test("Mark exactly the square", () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId('teste1'));
+    expect(getByTestId('teste1')).toHaveTextContent('O')
+  })
+  test('Alternate clicks generate different simbols', () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId('teste1'));
+    expect(getByTestId('teste1')).toHaveTextContent('O')
+    fireEvent.click(getByTestId('teste2'));
+    expect(getByTestId('teste2')).toHaveTextContent('X')
+    fireEvent.click(getByTestId('teste3'));
+    expect(getByTestId('teste3')).toHaveTextContent('O')
+    fireEvent.click(getByTestId('teste4'));
+    expect(getByTestId('teste4')).toHaveTextContent('X')
+  })
+  test('Recognizing horizontal win conditions', () => {
+    const { getByTestId, getByText } = render(<App />);
+    fireEvent.click(getByTestId('teste1'));
+    fireEvent.click(getByTestId('teste4'));
+    fireEvent.click(getByTestId('teste2'));
+    fireEvent.click(getByTestId('teste5'));
+    fireEvent.click(getByTestId('teste3'));
+    expect(getByText('Ganhou!')).not.toBeNull();
+  })
+  test('Recognizing vertical win conditions', () => {
+    const { getByTestId, getByText } = render(<App />);
+    fireEvent.click(getByTestId('teste1'));
+    fireEvent.click(getByTestId('teste2'));
+    fireEvent.click(getByTestId('teste4'));
+    fireEvent.click(getByTestId('teste3'));
+    fireEvent.click(getByTestId('teste7'));
+    expect(getByText('Ganhou!')).not.toBeNull();
+  })
+  test('Recognizing diagonal win conditions', () => {
+    const { getByTestId, getByText } = render(<App />);
+    fireEvent.click(getByTestId('teste1'));
+    fireEvent.click(getByTestId('teste2'));
+    fireEvent.click(getByTestId('teste5'));
+    fireEvent.click(getByTestId('teste3'));
+    fireEvent.click(getByTestId('teste9'));
+    expect(getByText('Ganhou!')).not.toBeNull();
+  })
+})
+
+describe('Edge Cases', () => {
+  test("Not change an alredy marked square", () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId("teste1"));
+    fireEvent.click(getByTestId("teste1"));
+    expect(getByTestId('teste1')).toHaveTextContent('O');
+  })
+  test("Finish game after a winner", () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId("teste1"));
+    fireEvent.click(getByTestId("teste2"));
+    fireEvent.click(getByTestId("teste5"));
+    fireEvent.click(getByTestId("teste6"));
+    fireEvent.click(getByTestId("teste9"));
+    fireEvent.click(getByTestId("teste3"));
+    fireEvent.click(getByTestId("teste4"));
+    fireEvent.click(getByTestId("teste7"));
+    fireEvent.click(getByTestId("teste8"));
+    expect(getByTestId('teste3')).toHaveTextContent('')
+    expect(getByTestId('teste4')).toHaveTextContent('')
+    expect(getByTestId('teste7')).toHaveTextContent('')
+    expect(getByTestId('teste8')).toHaveTextContent('')
+  })
+})
